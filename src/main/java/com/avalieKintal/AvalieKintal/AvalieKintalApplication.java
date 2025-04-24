df78e5206a3b1387c82f3c4c9583eef3f47d3293
JoaoCustodio66 <joaocustodiokintal@gmail.com>
2025-04-24 18:13:38 -0300

    First commit with SpringBoot  configurations

---
+++ src/main/java/com/avalieKintal/AvalieKintal/AvalieKintalApplication.java
@@ -0,0 +1,13 @@
+package com.avalieKintal.AvalieKintal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalieKintalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvalieKintalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avalieKintal/AvalieKintal/AvalieKintalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avalieKintal.AvalieKintal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalieKintalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
